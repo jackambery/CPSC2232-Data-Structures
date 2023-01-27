2ebc988c63e9d2aa96ffd776a414a0375b1c8c55
Jack Ambery <jack.ambery@gmail.com>
2023-01-27 14:23:12 -0500

    commented out

--- SelectionSort/src/TestHarness.java
+++ SelectionSort/src/TestHarness.java
@@ -21,20 +21,20 @@ public class TestHarness {
 		System.out.println();
 	}
 
-	public static void TEST_BinarySearch(SelSortBinSearch tester) {
-		System.out.println("Binary Search:");
-		System.out.println("0 is in array: " + tester.binarySearch(0, 0, SelSortAndBinSearch.SIZE - 1));
-		System.out.println("1 is in array: " + tester.binarySearch(1, 0, SelSortAndBinSearch.SIZE - 1));
-
-		for (int i = (SelSortAndBinSearch.SIZE / 2 - 2); i <= (SelSortAndBinSearch.SIZE / 2 + 2); i++)
-			System.out.println(i + " is in array: " + tester.binarySearch(i, 0, SelSortAndBinSearch.SIZE - 1));
-
-		System.out.println(SelSortAndBinSearch.SIZE - 1 + " is in array: "
-				+ tester.binarySearch(SelSortAndBinSearch.SIZE - 1, 0, SelSortAndBinSearch.SIZE - 1));
-		System.out.println(SelSortAndBinSearch.SIZE + " is in array: "
-				+ tester.binarySearch(SelSortAndBinSearch.SIZE, 0, SelSortAndBinSearch.SIZE - 1));
-		System.out.println();
-
-	}
+//	public static void TEST_BinarySearch(SelSortBinSearch tester) {
+//		System.out.println("Binary Search:");
+//		System.out.println("0 is in array: " + tester.binarySearch(0, 0, SelSortAndBinSearch.SIZE - 1));
+//		System.out.println("1 is in array: " + tester.binarySearch(1, 0, SelSortAndBinSearch.SIZE - 1));
+//
+//		for (int i = (SelSortAndBinSearch.SIZE / 2 - 2); i <= (SelSortAndBinSearch.SIZE / 2 + 2); i++)
+//			System.out.println(i + " is in array: " + tester.binarySearch(i, 0, SelSortAndBinSearch.SIZE - 1));
+//
+//		System.out.println(SelSortAndBinSearch.SIZE - 1 + " is in array: "
+//				+ tester.binarySearch(SelSortAndBinSearch.SIZE - 1, 0, SelSortAndBinSearch.SIZE - 1));
+//		System.out.println(SelSortAndBinSearch.SIZE + " is in array: "
+//				+ tester.binarySearch(SelSortAndBinSearch.SIZE, 0, SelSortAndBinSearch.SIZE - 1));
+//		System.out.println();
+//
+//	}
 
 }
