f67e6b4d1fdcfdc8014def1a56d77bc86124da0a
Jack Ambery <jack.ambery@gmail.com>
2023-02-01 15:16:37 -0500

    hw2 working properly, no bugs

--- HW2LinkedList/src/ListImpl.java
+++ HW2LinkedList/src/ListImpl.java
@@ -10,7 +10,7 @@ public class ListImpl<T extends Comparable<T>> implements List<T> {
 	// insert adds a node into the list in key order
 	public boolean insert(T key) {
 		if (head == null) {
-			head = new ListNode(key, null);
+			head = new ListNode<T>(key, null);
 			return true;
 		}
 		// TODO if list is empty, create a new ListNode with key and null as parameters
@@ -27,15 +27,17 @@ public class ListImpl<T extends Comparable<T>> implements List<T> {
 		if (prev != null) {
 			// we insert after the prev node
 			ListNode<T> nextOfPrev = prev.getNext();
-			ListNode<T> temp = new ListNode(key, nextOfPrev);
-			nextOfPrev = temp;
+			ListNode<T> temp = new ListNode<T>(key, nextOfPrev);
+			// nextOfPrev = temp;
+			prev.setNext(temp);
+			;
 
 		}
 		// TODO create a new ListNode with key and head as parameters // set head to the
 		// new node
 		else {
 			// we insert a new first node
-			ListNode<T> newHead = new ListNode(key, head);
+			ListNode<T> newHead = new ListNode<T>(key, head);
 			head = newHead;
 			return true;
 		}
@@ -61,32 +63,44 @@ public class ListImpl<T extends Comparable<T>> implements List<T> {
 
 	public boolean remove(T key) {
 		ListNode<T> prev = search(key);
-		// TODO if head is null you can return false (nothing in the list)
-		if (prev == null) // potentially removing 1st node
-		{
 
-			// TODO if head is null you can return false (nothing in the list)
-			if (head == null)
-				return false;
+		// TODO if head is null you can return false (nothing in the list)
+		if (head == null) {
+			return false;
 		}
 		// TODO
 		// if head.data is same as key
 		// set head to head next
 		// return true
 		// else return false – no match
-		else { // prev != null
-				// TODO
-				// if prev next is null
-				// there is nothing to remove
-				// return false
-				// TODO
-				// if prev next data matches the key
-				// create local reference del to the prev next node
-				// set prev next to the del next reference to bypass the node // return true
-				// TODO
-				// else return false
+		if (head.getData() == key) {
+			head = head.getNext();
+			return true;
 		}
-		return false; // remove
+		// TODO
+		// if prev next is null
+		// there is nothing to remove
+		// return false
+		if (prev != null) {
+			if (prev.getNext() == null) {
+				return false;
+			}
+			
+			// TODO
+			// if prev next data matches the key
+			// create local reference del to the prev next node
+			// set prev next to the del next reference to bypass the node // return true
+			if (prev.getNext().getData() == key) {
+				ListNode<T> del = prev.getNext().getNext();
+				prev.setNext(del);
+				return true;
+			}
+		}
+		
+		// TODO
+		// else return false
+		return false;
+
 	}
 
 	// TODO
@@ -95,8 +109,13 @@ public class ListImpl<T extends Comparable<T>> implements List<T> {
 		if (head != null) {
 			ListNode<T> current = head;
 			System.out.print("[");
-			while (current.getNext() != null) {
-				System.out.print(current.getData());
+			while (current != null) {
+				// if/else for removing last comma
+				if (current.getNext() == null) {
+					System.out.print(current.getData());
+				} else {
+					System.out.print(current.getData() + ", ");
+				}
 				current = current.getNext();
 			}
 			System.out.println("]");
