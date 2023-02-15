609fc032aee3b7a6bc37d8230f4c3d96e9ea7dc6
Jack Ambery <jack.ambery@gmail.com>
2023-02-15 14:14:22 -0500

    String tokenizer done - not tested yet

--- RPNProject/src/RPNStringTokenizer.java
+++ RPNProject/src/RPNStringTokenizer.java
@@ -7,8 +7,48 @@ public interface RPNStringTokenizer {
 		// - then put them in a list for processing.
 		// anything bad (not int or one of the operators we like) and return a null.
 
-		// YOU WRITE THIS!
-		return null;
+		ArrayList<Character> operators = new ArrayList<Character>();
+		operators.add('+');
+		operators.add('-');
+		operators.add('*');
+		operators.add('/');
+		operators.add('^');
+		
+		char[] chars = expression.toCharArray();
+		int tracker = 0;
+		for (char c : chars) {
+			if (Character.isDigit(c)) {
+				tracker++;
+				break;
+			}
+			//valid time to use expression
+			if (operators.contains(c) && tracker == 2) {
+				tracker = 0;
+				break;
+			}
+			//too many operands
+			if (operators.contains(c) && tracker != 2) {
+				System.out.println(expression + "is not a valid expression");
+				return null;
+			}
+			//invalid operator
+			if (!operators.contains(c)) {
+				System.out.println(expression + "is not a valid expression");
+				return null;
+			}
+			
+		}
+		if (tracker == 0 ) {
+			ArrayList<String> validExp = new ArrayList<String>();
+			for (char c : chars) {
+				validExp.add(Character.toString(c));
+			}
+			return validExp;
+		}
+		else {
+			return null;
+		}
+		
 
 	}
 }
