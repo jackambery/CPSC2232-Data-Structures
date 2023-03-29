fcfae9313a52edbfec323fb1c45fd5fd39154d3b
Jack Ambery <jack.ambery@gmail.com>
2023-03-29 11:29:48 -0400

    in class code

---
+++ TreeTraversal/src/TreeNode.java
@@ -0,0 +1,16 @@
+
+import java.util.Vector;
+
+ 
+public class TreeNode {
+	 
+	protected int key;
+	protected Vector<TreeNode> child = new Vector<>();
+
+	protected TreeNode (int key)
+	{
+	    this.key = key;
+	}
+		
+	
+}
